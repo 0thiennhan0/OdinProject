0f77b8e5b55f06f54b2617718e0fb42cdb141894
0thiennhan0 <hntnhan97@gmail.com>
2021-12-03 11:53:05 +0700

    Create basic tictactoe game

---
+++ Tic Tac Toe/public/javascript/main.js
@@ -0,0 +1,148 @@
+let gameType = "tictactoe";
+
+const gameBoard = ((gameType) => {
+  let _board = gameType === "tictactoe" ? [...Array(9)] : [...Array(64)];
+  const _boardContainer = document.querySelector(`#game-board`);
+
+  const getBoard = () => _board;
+
+  const getBoardIndex = (index) => _board[index];
+
+  const setSignByIndex = (index, sign, box) => {
+    if (_board[index] === undefined) {
+      _board[index] = sign;
+      const signContianer = document.createElement("p");
+      signContianer.textContent = sign;
+      signContianer.classList.add("sign-container");
+      signContianer.style.fontSize = `${(box.offsetWidth * 3) / 5 / 16}rem`;
+      box.appendChild(signContianer);
+      return true;
+    }
+    return false;
+  };
+
+  const resetBoard = () => {
+    _board = gameType === "tictactoe" ? [...Array(9)] : [...Array(64)];
+    _board.innerHTML = "";
+    _initBoard();
+  };
+
+  const _initBoard = (() => {
+    for (let i = 0; i < _board.length; i++) {
+      let box = document.createElement("div");
+      box.classList.add("box");
+      box.setAttribute("date-index", i);
+      _boardContainer.appendChild(box);
+    }
+  })();
+
+  return {
+    getBoard,
+    getBoardIndex,
+    setSignByIndex,
+    resetBoard,
+  };
+})(gameType);
+
+const Player = (sign, name) => {
+  return {
+    sign,
+    name,
+  };
+};
+
+const displayController = (() => {})();
+
+const gameController = (() => {
+  const _player1 = Player("X", "Player1");
+  const _player2 = Player("O", "Player2");
+  let turn = 0;
+
+  let _boxes = document.querySelectorAll(".box");
+  const addBoxEventListener = () => {
+    for (let box of _boxes) {
+      box.addEventListener("click", () => {
+        const checkBox = gameBoard.setSignByIndex(
+          box.getAttribute("date-index"),
+          turn % 2 ? _player2.sign : _player1.sign,
+          box,
+        );
+        if (checkBox) {
+          turn++;
+          if (checkGameOver()) alert(checkGameOver().name);
+        }
+      });
+    }
+  };
+
+  const checkGameOver = (match = 3) => {
+    const board = gameBoard.getBoard();
+    const edge = board.length ** 0.5;
+
+    //check match by row
+    for (let i = 0; i < board.length; i += edge) {
+      let count = 1;
+      let sign;
+      for (let j = i; j < i + edge; j++) {
+        if (board[j] && board[j] === board[j + 1]) {
+          if (++count === match) return _player1.sign === sign ? _player1 : _player2;
+          sign = board[j];
+        } else {
+          count = 1;
+          break;
+        }
+      }
+    }
+
+    // check match by column
+    for (let i = 0; i < edge; i++) {
+      let count = 1;
+      let sign;
+      for (let j = i; j < board.length; j += edge) {
+        if (board[j] && board[j] === board[j + edge]) {
+          if (++count === match) return _player1.sign === sign ? _player1 : _player2;
+          sign = board[j];
+        } else {
+          count = 1;
+        }
+      }
+    }
+
+    // check match by diagonal
+    for (let i = 0; i <= edge - match; i++) {
+      for (let j = i; j <= (edge - match) * edge; j += edge) {
+        let countL = 1;
+        let signL;
+        let countR = 1;
+        let signR;
+        for (let k = j; k < board.length; k += edge + 1) {
+          if (board[k] && board[k] === board[k + edge + 1]) {
+            if (++countL === match) return _player1.sign === signL ? _player1 : _player2;
+            signL = board[k];
+          } else {
+            countL = 1;
+          }
+        }
+        for (let k = j + edge - i * 2 - 1; k < board.length; k += edge - 1) {
+          if (board[k] && board[k] === board[k + edge - 1]) {
+            if (++countR === match) return _player1.sign === signR ? _player1 : _player2;
+            signR = board[k];
+          } else {
+            countR = 1;
+          }
+        }
+      }
+    }
+
+    return false;
+  };
+
+  return {
+    addBoxEventListener,
+  };
+})();
+
+// Start Game
+(() => {
+  gameController.addBoxEventListener();
+})();
